fcaf38b4f7b7cfea05e65d42f37c8efdb74331ab
emobilis <clency2023@gmail.com>
2023-03-02 09:41:40 +0300

    added country

--- djangoCRUD123/models.py
+++ djangoCRUD123/models.py
@@ -5,6 +5,7 @@ class Student(models.Model):
     email = models.EmailField()
     age = models.IntegerField()
     gender = models.CharField(max_length=50, blank=False, null=False)
+    country = models.CharField(default='Kenya', max_length=50)
 
 def __str__(self):
     return self.name
